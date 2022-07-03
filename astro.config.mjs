e118b11e187c9b6788ff1767176d51e2558d267a
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-07-02 21:58:11 -0400

    tweak config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kevinzunigacuellar.github.io/',
-  base: 'astro-gh-pages',
+  site: 'https://kevinzunigacuellar.github.io',
+  base: '/astro-gh-pages',
 });
